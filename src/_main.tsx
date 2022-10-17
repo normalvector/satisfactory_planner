1b707fd432b8306cc6b27a4d331d6cf60982783a
Paul Golds <Paul.Golds@bbc.co.uk>
2022-10-17 19:30:14 +0100

    Have simple React renderer

--- src/_main.tsx
+++ src/_main.tsx
@@ -1,2 +1,13 @@
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+
 const s: string = "Hello word"
 console.log(`I said ${s}`)
+
+const rootElement = document.getElementById('app')
+if (!rootElement) {
+    console.error("Cannot find HTML div with ID 'app'")
+} else {
+    const root = createRoot(rootElement)
+    root.render(<h1>Hello World</h1>)
+}
